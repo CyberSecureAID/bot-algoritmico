0a2a6a6d18778d8e20e35fde48ec6130a795200f
Jesus | Developer & Trader <104212122+CyberSecureAID@users.noreply.github.com>
2026-09-11 21:48:09 -0600

    Add files via upload

--- assets/portada/portada.js
+++ assets/portada/portada.js
@@ -632,7 +632,7 @@ try {
 (async () => {
   let idi;
   try {
-    idi = await import(J + 'idioma.js?v=148');
+    idi = await import(J + 'idioma.js?v=149');
     idi.arrancarIdioma();
   } catch (e) {
     console.warn('[portada] idioma:', e);
