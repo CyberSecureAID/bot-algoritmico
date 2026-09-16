cf30c42cc23fce25918a50516ee3d2ef53aa5f51
Jesus | Developer & Trader <104212122+CyberSecureAID@users.noreply.github.com>
2026-09-16 13:32:47 -0600

    Add files via upload

--- assets/js/movil/movil.js
+++ assets/js/movil/movil.js
@@ -75,7 +75,7 @@ async function abrir(clave, arg) {
   try {
     switch (clave) {
       case 'swap':      { inyectarFixSwap(); const m = await import('../gridbot/swap.js?v=5'); m.abrirSwap && m.abrirSwap(); sacarSwapDelWeb();
-                          try { const idi = await import('../idioma.js?v=160'); idi.traducirTodo && idi.traducirTodo(); } catch (_) {} break; }
+                          try { const idi = await import('../idioma.js?v=161'); idi.traducirTodo && idi.traducirTodo(); } catch (_) {} break; }
       case 'polvo':     await abrirToolDirecto('polvo'); break;
       case 'alerta':    abrirAlerta(); break;
       case 'alertas':   abrirAlerta(); break;
