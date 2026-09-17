4038549dc6b79786fc258c747a3a046ca744c530
Jesus | Developer & Trader <104212122+CyberSecureAID@users.noreply.github.com>
2026-09-16 21:39:21 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   · Si hay versión nueva, se descarga sola y se aplica al recargar.
  */
 
-const VERSION = 'aurex-v235';
+const VERSION = 'aurex-v236';
 const APP = [
   './',
   './index.html',              // la portada
