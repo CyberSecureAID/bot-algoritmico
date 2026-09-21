fe0b65345c0a521a298157193383a80bd97cca18
Jesus | Developer & Trader <104212122+CyberSecureAID@users.noreply.github.com>
2026-09-21 13:33:12 -0600

    Add files via upload

--- assets/js/gridbot-ui.js
+++ assets/js/gridbot-ui.js
@@ -5,7 +5,7 @@
  * botones (i), gráfica viva con las cuadrículas, e inversión total en una cifra.
  */
 
-import * as gb from './gridbot.js?v=125';
+import * as gb from './gridbot.js?v=126';
 import { t } from './idioma.js?v=163';
 import * as wallet from './wallet.js?v=125';
 import { MONEDAS, LISTA_TODAS } from './tokens.js?v=125';
